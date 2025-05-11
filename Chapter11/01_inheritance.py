decb900bbee0609e33220bd4978710fba355ec79
pardeshiabhay <pardeshiabhay999@gmail.com>
2025-05-11 16:15:56 +0530

    Inheritance & More on OOP's

---
+++ Chapter11/01_inheritance.py
@@ -0,0 +1,29 @@
+class Employee:
+    company = "ITC"
+    def show(self):
+        print(f"The name of employee is {self.name} and the salary is {self.salary}")
+    
+
+# class Programmer:
+#     company = "ITC Infotech"
+#     def show(self):
+#         print(f"The name of employee is {self.name} and the salary is {self.salary}")
+#     def showLanguage(self):
+#         print(f"The name of employee is {self.name} and he is good with {self.language} language")
+        
+#Inheritance
+class Programmer(Employee):
+    company = "ITC Infotech"
+    def show(self):
+       print(f"The name of employee is {self.name} and the salary is {self.salary}")
+    
+    
+
+
+
+a = Employee()
+b = Programmer()
+
+print(a.company, b.company)
+    
+    
